c61e367172e6de93e0a8ce20f2a152cddd1a8854
Georgii <gmnaumov@yandex.ru>
2022-11-24 21:40:27 +0300

    feat:Added simple JS scripts

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,34 @@
-const myHeading = document.querySelector("h1")
-myHeading.textContent = "Hello, JavaScript!"
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+    const myScr = myImage.getAttribute("src");
+    if (myScr === "images/logo.png") {
+        myImage.setAttribute("src", "images/logo2.png")
+    } else {
+        myImage.setAttribute("src", "images/logo.png");
+    }
+};
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    }
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function () {
+    setUserName();
+}
